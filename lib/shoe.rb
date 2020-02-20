34ab07b697ce66e2fbd22e1d29976b344331118b
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-20 01:15:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition, :cobble
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def initialize(brand)
     @brand = brand
